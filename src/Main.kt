8d52b5258decef8f0cf4cb4bbf533d5b68756a3b
Jean carlos <jeancarloslunachaguendo2005@gmail.com>
2025-08-20 14:17:35 -0500

    Curso

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,162 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    println("Hello, Android!")
-
-    println("Today is sunny!")
-
+//Ejercicio 1
+/*fun main() {
     println("1")
     println("2")
     println("3")
-
+}
+//Ejercicio 2
+fun main() {
     println("I'm")
-    println("Learning")
-    println("kotlin!")
-
+    println("learning")
+    println("Kotlin!")
+}
+//Ejercicio 3
+fun main() {
     println("Monday")
     println("Tuesday")
     println("Wednesday")
     println("Thursday")
     println("Friday")
 }
+//Ejercicio 4
+fun main() {
+    println("Tomorrow is rainy")
+}
+//Ejercicio 5
+fun main() {
+    println("There is a chance of snow")
+}
+//Ejercicio 6
+fun main() {
+    println("Cloudy")
+    println("Partly Cloudy")
+    println("Windy")
+}
+//Ejercicio 7
+fun main() {
+    println("How's the weather today?")
+}
+//Ejercicio 8
+fun main() {
+    val count: Int = 2
+    println(count)
+}
+fun main() {
+    val count: Int = 2
+    println("You have count unread messages.")
+}
+fun main() {
+    val count: Int = 2
+    println("You have $count unread messages.")
+}
+fun main() {
+    val count: Int = 10
+    println("You have $count unread messages.")
+}
+//  Ejercicio 9
+fun main() {
+    val unreadCount = 5
+    val readCount = 100
+    println("You have ${unreadCount + readCount} total messages in your inbox.")
+}
+fun main() {
+    val numberOfPhotos = 100
+    val photosDeleted = 10
+    println("$numberOfPhotos photos")
+    println("$photosDeleted photos deleted")
+    println("${numberOfPhotos - photosDeleted} photos left")
+}
+//Ejercicio 10
+fun main() {
+    var cartTotal = 0
+    cartTotal = 20
+    println("Total: $cartTotal")
+}
+fun main() {
+    var cartTotal = 0
+    println("Total: $cartTotal")
 
+    cartTotal = 20
+    println("Total: $cartTotal")
+}
+fun main() {
+    val count: Int = 10
+    println("You have $count unread messages.")
+}
+fun main() {
+    var count: Int = 10
+    println("You have $count unread messages.")
+}
+fun main() {
+    var count = 10
+    println("You have $count unread messages.")
+    count = count + 1
+}
+fun main() {
+    var count = 10
+    println("You have $count unread messages.")
+    count = count + 1
+    println("You have $count unread messages.")
+}
+fun main() {
+    var count = 10
+    println("You have $count unread messages.")
+    count++
+    println("You have $count unread messages.")
+}
+fun main() {
+    var count = 10
+    println("You have $count unread messages.")
+    count--
+    println("You have $count unread messages.")
+}
+fun main() {
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLength: Double = 0.0
+    println("$totalTripLength miles left to destination")
+}
+fun main() {
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLength: Double = trip1 + trip2 + trip3
+    println("$totalTripLength miles left to destination")
+}
+fun main() {
+    val trip1 = 3.20
+    val trip2 = 4.10
+    val trip3 = 1.72
+    val totalTripLength = trip1 + trip2 + trip3
+    println("$totalTripLength miles left to destination")
+}
+fun main() {
+    val nextMeeting = "Next meeting:"
+    val date = "January 1"
+    val reminder = nextMeeting + date
+    println(reminder)
+}
+fun main() {
+    val nextMeeting = "Next meeting: "
+    val date = "January 1"
+    val reminder = nextMeeting + date + " at work"
+    println(reminder)
+}
+fun main() {
+    println("Say \"hello\"")
+}
+fun main() {
+    val notificationsEnabled: Boolean = true
+    println(notificationsEnabled)
+}
+fun main() {
+    val notificationsEnabled: Boolean = false
+    println(notificationsEnabled)
+}
+fun main() {
+    val notificationsEnabled: Boolean = false
+    println("Are notifications enabled? " + notificationsEnabled)
+}*/
