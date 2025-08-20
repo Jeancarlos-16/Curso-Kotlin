ba021d6ec2f0807b18b32cbdcf868ac886e71c17
Jean carlos <jeancarloslunachaguendo2005@gmail.com>
2025-08-20 11:42:04 -0500

    Curso

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     println("Hello, Android!")
+
+    println("Today is sunny!")
+
+    println("1")
+    println("2")
+    println("3")
+
+    println("I'm")
+    println("Learning")
+    println("kotlin!")
+
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
 }
+
